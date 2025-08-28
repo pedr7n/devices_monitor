29749125dd8136d6d56f422a039ee05ed820a8bb
pedr7n <pedrotfm68@gmail.com>
2025-08-28 09:43:44 -0300

    fixed redirect on login_view and added .gitignore file

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,40 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 def register_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect('login')
+            messages.success(request, "Conta criada com sucesso! Faça login.")
+            return redirect("login")
+        else:
+            messages.error(request, "Corrija os erros abaixo.")
     else:
         user_form = UserCreationForm()
-    return render(request, 'register.html', {'user_form': user_form})
+
+    return render(request, "register.html", {"user_form": user_form})
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+    if request.method == "POST":
+        login_form = AuthenticationForm(request, data=request.POST)
+        if login_form.is_valid():
+            user = login_form.get_user()
             login(request, user)
-            return redirect('devices_list')
+            return redirect("device_list")
         else:
-            login_form = AuthenticationForm()
-
+            messages.error(request, "Usuário ou senha inválidos.")
     else:
         login_form = AuthenticationForm()
-    return render(request, 'login.html', {'login_form': login_form})
+
+    return render(request, "login.html", {"login_form": login_form})
+
 
 def logout_view(request):
     logout(request)
-    return redirect('devices_list')
+    messages.info(request, "Você saiu da sua conta.")
+    return redirect("login")
